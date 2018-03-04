f7394e7fbff974c43765371692542140c2701973
Muhammad Owais Bawany <muhammadowaisbawany@gmail.com>
2018-03-03 21:23:33 -0500

    fixeed synchronization issues

--- Planting.java
+++ Planting.java
@@ -39,53 +39,90 @@ class Student extends Thread
 	}
 
 
+
 	public void run()
 	{
+		try{
+		ta.max_hole.acquire();}
+		catch(InterruptedException e9)
+		{e9.printStackTrace();}
+		
 
 		while(true)
 		{
 
-		try{
-			ta.max_hole.acquire(); 
-			// if (ta.max_hole.availablePermits() == 0){
-		     	// ta.shovel_share.release(1);
-		 		// }	
-		     // Can dig a hole - lets get the shovel
-		    //try acqure uniterruptible if just try acquire doesnt work 
-		    try{
-		     ta.shovel_share.acquireUninterruptibly(1);
+		// if (ta.permmissionForStudent.tryAcquire() == true){
+		// try{
+		// 	ta.permmissionForStudent.acquire();
 
-		     System.out.println("Student: Got the shovel");
-	             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
-                     ta.incrHoleDug();  // hole filled - increment the number	
-		     System.out.println("Student: Hole "+ta.getHoleDug()+" Dug");
-		     System.out.println("Student: Letting go of the shovel");
-		     ta.empty_hole.release();
-		     ta.shovel_share.release();
-		 
-		 }
-		 finally {
-		 	ta.shovel_share.release();
-		 }
-		}
-		
-		catch(InterruptedException e1)
-			{
-			System.out.println("Student: Must wait for TA "+ta.getMAX()+" holes ahead\n");
-			//e1.printStackTrace();
-			break;
+			try{
+				if(ta.max_hole.availablePermits()==0)
+					System.out.println("Student: Must wait for TA "+ta.getMAX()+" holes ahead");
+				ta.max_hole.acquire(); 
+				// if (ta.max_hole.availablePermits() == 0){
+			     	// ta.shovel_share.release(1);
+			 		// }	
+			     // Can dig a hole - lets get the shovel
+			    //try acqure uniterruptible if just try acquire doesnt work 
+
+			  	try{
+				     ta.shovel_share.acquireUninterruptibly();
+
+				     System.out.println("Student: Got the shovel");
+			             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
+	                     ta.incrHoleDug();  // hole filled - increment the number	
+			     System.out.println("Student: Hole "+ta.getHoleDug()+" Dug");
+			     System.out.println("Student: Letting go of the shovel");
+
+			 }
+			 finally{
+			     ta.shovel_share.release();
+			 }
 			}
-		
-		finally {
-			 ta.unfilled_hole.release();
-		     if(isInterrupted()) break;
-		}
-		
+			catch(InterruptedException e9)
+				{e9.printStackTrace();}
+			finally{
+				     ta.empty_hole.release();
+ 					}
+			if(isInterrupted()) break;
+			 // }
+			 // finally {
+			 // 	ta.shovel_share.release();
+			 // }
+			}
+			
+		// 	catch(InterruptedException e1)
+		// 		{
+		// 		System.out.println("Student: Must wait for TA "+ta.getMAX()+" holes ahead\n");
+		// 		//e1.printStackTrace();
+		// 		// break;
+		// 		}
+
+		// 	//if(isInterrupted()) break;
+
+
+
+		// 	// if(isInterrupted()) break;
+		// 	// finally {
+		// 	// 	 ta.unfilled_hole.release();
+		//       if(isInterrupted()) break;
+		// 	// }
+		// }
+		// catch(InterruptedException e111)
+		// {
+		// 	break;
+		// }
+
+		// finally{
+		// 		ta.permmissionForStudent.release();
+		// 		}	
 	
-		}
+		// }
+		// }
 		System.out.println("Student is done");
-		}
-		}
+
+	}
+}
 
 
 
@@ -109,6 +146,9 @@ class TA extends Thread
 	Semaphore unfilled_hole;
 	Semaphore max_hole;
 	Semaphore shovel_share;
+
+			//add a permmission from prof to keep working 
+	Semaphore permmissionForStudent;
 	
 
 	public int getMAX() { return(MAX); }
@@ -132,35 +172,61 @@ class TA extends Thread
 		max_hole		= new Semaphore(5, true);
 		shovel_share	= new Semaphore(1, true);
 
+		//add a permmission from prof to keep working 
+		permmissionForStudent = new Semaphore(1, true);
+
 	}
 	
 	public void run()
 	{
 		// available.acquire();
-		try{
-		unfilled_hole.acquire();}
-		catch(InterruptedException e2)
-		{e2.printStackTrace();}
+		// try{
+		// unfilled_hole.acquire();
+	// }
+	// 	catch(InterruptedException e2)
+	// 	{e2.printStackTrace();}
 		
 		while(true)
 		{	
-			shovel_share.acquireUninterruptibly(1);
-			 // if (ta_semaphore.tryacquire() == 1){
-			 	System.out.println("TA: Got the shovel");
-	            // ta_semaphore.acquire();
-	            // shovel_share.tryacquire();
+			try{
+				unfilled_hole.acquire();
+				try{
+					shovel_share.acquireUninterruptibly();
+				 // if (ta_semaphore.tryacquire() == 1){
+				 	System.out.println("TA: Got the shovel");
+		            // ta_semaphore.acquire();
+		            // shovel_share.tryacquire();
 
 
-	             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
-                     holeFilledNum++;  // hole filled - increment the number	
-		     System.out.println("TA: The hole "+holeFilledNum+" has been filled");
-		     System.out.println("TA: Letting go of the shovel");
-		     //unfilled_hole.release();
-		     shovel_share.release();
-		     max_hole.release();
-		     
-		     if(isInterrupted()) break;
-		}
+		             try {sleep((int) (100*Math.random()));} catch (Exception e) { break;} // Time to fill hole
+	                     holeFilledNum++;  // hole filled - increment the number	
+					     System.out.println("TA: The hole "+holeFilledNum+" has been filled");
+					     System.out.println("TA: Letting go of the shovel");
+
+					 }
+					     //unfilled_hole.release();
+					 finally{
+					     shovel_share.release();
+					 }
+					
+				}
+
+					 catch(InterruptedException e2)
+						{e2.printStackTrace();}
+					 finally{
+					     max_hole.release();
+					 }
+			
+			if(isInterrupted()) break;
+		//      {
+		//      	try{
+		//      	permmissionForStudent.acquire();
+		//      	}
+		//      finally{
+		//      	break;
+		//   }
+		// }
+	}
 		System.out.println("TA is done");
 
 	}
@@ -182,25 +248,34 @@ class Professor extends Thread
 	
 	public void run()
 	{
-		try {
-		ta.empty_hole.acquire();}
-		catch(InterruptedException e3)
-		{e3.printStackTrace();}
+		// try {
+		// ta.empty_hole.acquire();
+	// }
+	// 	catch(InterruptedException e3)
+	// 	{e3.printStackTrace();}
 
 		while(ta.getHolePlanted() <= 20)
 		{
+			try{
 				// TA.empty_hole.acquire();
+				ta.empty_hole.acquire();
 		  //    	TA.incrHolePlanted();
 
 	            try {sleep((int) (50*Math.random()));
-	            	ta.empty_hole.acquire();} catch (Exception e) { break;} // Time to plant
+	            	// ta.empty_hole.acquire();
+	            	} catch (Exception e) { break;} // Time to plant
                      ta.incrHolePlanted();  // the seed is planted - increment the number	
-		     System.out.println("Professor: All be advised that I have completed planting hole "+
+		    		 System.out.println("Professor: All be advised that I have completed planting hole "+
 				        ta.getHolePlanted());
-		    ta.unfilled_hole.release();
-		}
-
+				}
+			catch(InterruptedException e3)
+				{e3.printStackTrace();}
+			finally{
+			  ta.unfilled_hole.release();
+			}
+		// ta.permmissionForStudent.acquireUninterruptibly();
 		System.out.println("Professeur: We have worked enough for today");
 	}
+}
 }
 
